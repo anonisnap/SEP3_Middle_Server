9e3d4ee3eb28237c9f667d7811df9c0f9273c4b1
Young <andreasyoungandersen@gmail.com>
2021-12-05 20:45:54 +0100

    Added pre-made Services with no Imports

---
+++ src/main/java/com/group5/sep3/GrpcCommunication/Services/ItemLocationService.java
@@ -0,0 +1,228 @@
+package com.group5.sep3.GrpcCommunication.Services;
+
+import java.util.ArrayList;
+
+public class ItemLocationService extends ItemLocationImplBase {
+	ItemLocationModel model;
+
+	public ItemLocationService(ItemLocationModel model) {
+		this.model = model;
+	}
+
+	@Override
+	public void registerItemLocation(gItemLocation request, StreamObserver<gItemLocation> responseObserver) {
+		System.out.println("+ Receiving ItemLocation from Client");
+
+		// METHOD CALLING
+		// Extract Object from Request
+		ItemLocation requestItemLocation = getItemLocationFromRequest(request);
+
+		// Perform Model Call
+		ItemLocation returnItemLocation = null;
+		try {
+			returnItemLocation = model.register(requestItemLocation);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		if (returnItemLocation == null) {
+			returnItemLocation = requestItemLocation;
+		}
+
+		// == == == == == == == == == == == ==
+
+		// METHOD RESPONSE SECTION | Here we do a bit of Magic to return the requested stuff
+		// Create Response Object | Since the proto file calls for a gItemLocation as a return value, we instantiate a gItemLocation.Builder object to create a Response
+		gItemLocation.Builder response = gItemLocation.newBuilder();
+
+		// Set Values Of Response Object | This Simply copies the itemLocation's information into the response.ItemLocation
+		try {
+			parseAndMergeItemLocation(response, returnItemLocation);
+		} catch (InvalidProtocolBufferException e) {
+			e.printStackTrace();
+		}
+
+		// Link the Response to the Observer | This tells the responseObserver, that when it is completed, it should send back the response (which is a gItemLocation object)
+		responseObserver.onNext(response.build());
+
+		// Tell Observer the Method is finished, and let it return the Response
+		responseObserver.onCompleted();
+
+		System.out.println("- Receiving ItemLocation from Client");
+	}
+
+	@Override
+	public void getItemLocation(gItemLocation request, StreamObserver<gItemLocation> responseObserver) {
+		System.out.println("+ Sending ItemLocation to Client");
+
+		// Fetch the ItemLocation from Request
+		ItemLocation requestItemLocation = getItemLocationFromRequest(request);
+
+		// Fetch the ItemLocation from the Server
+		ItemLocation itemLocation = null;
+		try {
+			itemLocation = model.get(requestItemLocation);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// Ensure ItemLocation was retrieved. If itemLocation is null, Echo back the ItemLocation requested. Optionally throw an Error
+		if (itemLocation == null) {
+			itemLocation = requestItemLocation;
+//			throw new NullPointerException();
+		}
+
+		// Create Response Object
+		gItemLocation.Builder response = gItemLocation.newBuilder();
+
+		// Set Values Of Response Object
+		try {
+			parseAndMergeItemLocation(response, itemLocation);
+		} catch (InvalidProtocolBufferException e) {
+			e.printStackTrace();
+		}
+
+		// Link the Response to the Observer
+		responseObserver.onNext(response.build());
+
+		// Tell Observer the Method is finished, and let it return the Response
+		responseObserver.onCompleted();
+
+		System.out.println("- Sending ItemLocation to Client");
+	}
+
+	@Override
+	public void getAllItemLocation(gItemLocation request, StreamObserver<gItemLocationList> responseObserver) {
+		System.out.println("+ Sending all ItemLocations from Client");
+
+		List<gItemLocation> returnItemLocations = new ArrayList<>();
+
+		Collection<ItemLocation> itemLocations = new ArrayList<>();
+
+		// Parse Model ItemLocations to gRPC ItemLocations
+		try {
+			itemLocations = model.getAll();
+			for (ItemLocation itemLocation : itemLocations) {
+				gItemLocation.Builder builder = gItemLocation.newBuilder();
+				parseAndMergeItemLocation(builder, itemLocation);
+				returnItemLocations.add(builder.build()); // THIS LIKELY WON'T WORK. UNSURE OF HOW PARSEFROM IS IMPLEMENTED
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// Create Response Object
+		gItemLocationList.Builder response = gItemLocationList.newBuilder();
+
+		// Add the gItemLocations to the Response
+		response.addAllItemLocations(returnItemLocations); // Add ItemLocations to Builder
+
+		// Link the Response to the Observer
+		responseObserver.onNext(response.build());
+
+		// Tell Observer the Method is finished, and let it return the Response
+		responseObserver.onCompleted();
+
+		System.out.println("- Sending all ItemLocations from Client");
+	}
+
+	@Override
+	public void updateItemLocation(gItemLocation request, StreamObserver<gItemLocation> responseObserver) {
+		System.out.println("+ Updating ItemLocation");
+
+		// Fetch ItemLocation
+		ItemLocation requestItemLocation = getItemLocationFromRequest(request);
+
+		// Update ItemLocation on Server
+		ItemLocation returnItemLocation = null;
+		try {
+			returnItemLocation = model.update(requestItemLocation);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// Ensure ReturnItemLocation is not Null | Consider throwing an Error Instead
+		if (returnItemLocation == null) {
+			returnItemLocation = requestItemLocation;
+		}
+
+		// Create Response Object
+		gItemLocation.Builder response = gItemLocation.newBuilder();
+
+		// Add the gItemLocations to the Response
+		try {
+			parseAndMergeItemLocation(response, returnItemLocation);
+		} catch (InvalidProtocolBufferException e) {
+			e.printStackTrace();
+		}
+
+		// Link the Response to the Observer
+		responseObserver.onNext(response.build());
+
+		// Tell Observer the Method is finished, and let it return the Response
+		responseObserver.onCompleted();
+
+		System.out.println("- Updating ItemLocation");
+	}
+
+	@Override
+	public void removeItemLocation(gItemLocation request, StreamObserver<gItemLocation> responseObserver) {
+		System.out.println("+ Removing ItemLocation");
+
+		// Fetch ItemLocation
+		ItemLocation requestItemLocation = getItemLocationFromRequest(request);
+
+		// Remove ItemLocation on Server
+		ItemLocation returnItemLocation = null;
+		try {
+			returnItemLocation = model.remove(requestItemLocation);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// Ensure ReturnItemLocation is not Null | Consider throwing an Error Instead
+		if (returnItemLocation == null) {
+			returnItemLocation = requestItemLocation;
+		}
+
+		// Create Response Object
+		gItemLocation.Builder response = gItemLocation.newBuilder();
+
+		// Add the gItemLocations to the Response
+		try {
+			parseAndMergeItemLocation(response, returnItemLocation);
+		} catch (InvalidProtocolBufferException e) {
+			e.printStackTrace();
+		}
+
+		// Link the Response to the Observer
+		responseObserver.onNext(response.build());
+
+		// Tell Observer the Method is finished, and let it return the Response
+		responseObserver.onCompleted();
+
+		System.out.println("- Receiving ItemLocation");
+	}
+
+	private ItemLocation getItemLocationFromRequest(gItemLocation req) {
+		gItem x = req.getItem();
+		gLocation y = req.getLocation();
+		Item i = new Item(x.getId(), x.getItemName(), x.getLength(), x.getWidth(), x.getHeight(), x.getWeight());
+		Location l = new Location(y.getId(), y.getDescription());
+		return new ItemLocation(req.getId(), l, i, req.getAmount());
+	}
+
+	private void parseAndMergeItemLocation(gItemLocation.Builder builder, ItemLocation itemLocation) throws InvalidProtocolBufferException {
+		gItem x = parseAndMergeItem(gItem.newBuilder(), itemLocation.getItem()).build();
+		gLocation y = parseAndMergeLocation(gLocation.newBuilder(), itemLocation.getLocation()).build();
+		builder.setId(itemLocation.getId()).setItem(x).setLocation(y).setAmount(itemLocation.getAmount());
+	}
+
+	private gItem.Builder parseAndMergeItem(gItem.Builder builder, Item item) {
+		return builder.setId(item.getId()).setItemName(item.getItemName()).setHeight(item.getHeight()).setLength(item.getLength()).setWidth(item.getWidth()).setWeight(item.getWeight());
+	}
+
+	private gLocation.Builder parseAndMergeLocation(gLocation.Builder builder, Location location) {
+		return builder.setId(location.getId()).setDescription(location.getDescription());
+	}
+}
---
+++ src/main/java/com/group5/sep3/GrpcCommunication/Services/ItemService.java
@@ -0,0 +1,205 @@
+package com.group5.sep3.GrpcCommunication.Services;
+
+import java.util.ArrayList;
+
+public class ItemService extends ItemImplBase {
+	ItemModel model;
+
+	public ItemService(ItemModel model) {
+		this.model = model;
+	}
+
+	@Override
+	public void registerItem(gItem request, StreamObserver<gItem> responseObserver) {
+		System.out.println("+ Receiving Item from Client");
+
+		// METHOD CALLING
+		// Extract Object from Request
+		Item requestItem = getItemFromRequest(request);
+
+		// Perform Model Call
+		Item returnItem = null;
+		try {
+			returnItem = model.register(requestItem);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		if (returnItem == null) {
+			returnItem = requestItem;
+		}
+
+		System.out.println("\t>> Return Item :\n" + returnItem);
+
+		// == == == == == == == == == == == ==
+
+		// METHOD RESPONSE SECTION | Here we do a bit of Magic to return the requested stuff
+		// Create Response Object | Since the proto file calls for a gItem as a return value, we instantiate a gItem.Builder object to create a Response
+		gItem.Builder response = gItem.newBuilder();
+
+		// Set Values Of Response Object | This Simply copies the item's information into the response.Item
+		parseAndMergeItem(response, returnItem);
+
+		// Link the Response to the Observer | This tells the responseObserver, that when it is completed, it should send back the response (which is a gItem object)
+		responseObserver.onNext(response.build());
+
+		// Tell Observer the Method is finished, and let it return the Response
+		responseObserver.onCompleted();
+
+		System.out.println("- Receiving Item from Client");
+	}
+
+	@Override
+	public void getItem(gItem request, StreamObserver<gItem> responseObserver) {
+		System.out.println("+ Sending Item to Client");
+
+		// Fetch the Item from Request
+		Item requestItem = getItemFromRequest(request);
+
+		// Fetch the Item from the Server
+		Item returnItem = null;
+		try {
+			returnItem = model.get(requestItem);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// Ensure Item was retrieved. If item is null, Echo back the Item requested. Optionally throw an Error
+		if (returnItem == null) {
+			returnItem = requestItem;
+//			throw new NullPointerException();
+		}
+		System.out.println("\t>> Return Item :\n" + returnItem);
+
+		// Create Response Object
+		gItem.Builder response = gItem.newBuilder();
+
+		// Set Values Of Response Object
+		parseAndMergeItem(response, returnItem);
+
+		// Link the Response to the Observer
+		responseObserver.onNext(response.build());
+
+		// Tell Observer the Method is finished, and let it return the Response
+		responseObserver.onCompleted();
+
+		System.out.println("- Sending Item to Client");
+	}
+
+	@Override
+	public void getAllItem(gItem request, StreamObserver<gItemList> responseObserver) {
+		System.out.println("+ Sending all Items from Client");
+
+		List<gItem> returnItems = new ArrayList<>();
+
+		Collection<Item> items;
+
+		// Parse Model Items to gRPC Items
+		try {
+			items = model.getAll();
+			for (Item item : items) {
+				gItem.Builder builder = gItem.newBuilder();
+				parseAndMergeItem(builder, item);
+				returnItems.add(builder.build());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// Create Response Object
+		gItemList.Builder response = gItemList.newBuilder();
+
+		// Add the gItems to the Response
+		response.addAllItem(returnItems); // Add Items to Builder
+
+		// Link the Response to the Observer
+		responseObserver.onNext(response.build());
+
+		// Tell Observer the Method is finished, and let it return the Response
+		responseObserver.onCompleted();
+
+		System.out.println("- Sending all Items from Client");
+	}
+
+	@Override
+	public void updateItem(gItem request, StreamObserver<gItem> responseObserver) {
+		System.out.println("+ Updating Item");
+
+		// Fetch Item
+		Item requestItem = getItemFromRequest(request);
+
+		// Update Item on Server
+		Item returnItem = null;
+		try {
+			returnItem = model.update(requestItem);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// Ensure ReturnItem is not Null | Consider throwing an Error Instead
+		if (returnItem == null) {
+			returnItem = requestItem;
+		}
+
+		System.out.println("\t>> Return Item :\n" + returnItem);
+
+		// Create Response Object
+		gItem.Builder response = gItem.newBuilder();
+
+		// Add the gItems to the Response
+		parseAndMergeItem(response, returnItem);
+
+		// Link the Response to the Observer
+		responseObserver.onNext(response.build());
+
+		// Tell Observer the Method is finished, and let it return the Response
+		responseObserver.onCompleted();
+
+		System.out.println("- Updating Item");
+	}
+
+	@Override
+	public void removeItem(gItem request, StreamObserver<gItem> responseObserver) {
+		System.out.println("+ Removing Item");
+
+		// Fetch Item
+		Item requestItem = getItemFromRequest(request);
+
+		// Remove Item on Server
+		Item returnItem = null;
+		try {
+			returnItem = model.remove(requestItem);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// Ensure ReturnItem is not Null | Consider throwing an Error Instead
+		if (returnItem == null) {
+			returnItem = requestItem;
+		}
+
+		System.out.println("\t>> Return Item :\n" + returnItem);
+
+		// Create Response Object
+		gItem.Builder response = gItem.newBuilder();
+
+		// Add the gItems to the Response
+		parseAndMergeItem(response, returnItem);
+
+		// Link the Response to the Observer
+		responseObserver.onNext(response.build());
+
+		// Tell Observer the Method is finished, and let it return the Response
+		responseObserver.onCompleted();
+
+		System.out.println("- Removing Item");
+	}
+
+	private Item getItemFromRequest(gItem req) {
+		return new Item(req.getId(), req.getItemName(), req.getLength(), req.getWidth(), req.getHeight(), req.getWeight());
+	}
+
+	private gItem.Builder parseAndMergeItem(gItem.Builder builder, Item item) {
+		return builder.setId(item.getId()).setItemName(item.getItemName()).setHeight(item.getHeight()).setLength(item.getLength()).setWidth(item.getWidth()).setWeight(item.getWeight());
+	}
+}
